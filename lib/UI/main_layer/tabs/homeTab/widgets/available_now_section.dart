509d4c6502aa59e972d3b852512ca2c712fb788c
MohamadQotb <mohamadbakoush@gmail.com>
2025-06-29 17:15:28 +0200

    edites on home tab ui

--- lib/UI/main_layer/tabs/homeTab/widgets/available_now_section.dart
+++ lib/UI/main_layer/tabs/homeTab/widgets/available_now_section.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movies_app/UI/main_layer/tabs/homeTab/widgets/watch_now_banner.dart';
 import '../../../../../core/utils/app_assets.dart';
 import '../../../../../core/utils/app_colors.dart';
 import 'data/movies_data.dart';
 
-class AvailableNowSection extends StatelessWidget {
+class AvailableNowSection extends StatefulWidget {
   const AvailableNowSection({super.key});
+
+  @override
+  State<AvailableNowSection> createState() => _AvailableNowSectionState();
+}
+
+class _AvailableNowSectionState extends State<AvailableNowSection> {
+  final PageController _pageController =
+      PageController(initialPage: 1, viewportFraction: 0.6);
+  int _currentIndex = 1;
+  String bgImage = availableNowMovies[1]['image']!;
 
   @override
   Widget build(BuildContext context) {
@@ -14,90 +25,160 @@ class AvailableNowSection extends StatelessWidget {
       children: [
         ShaderMask(
           shaderCallback: (rect) {
-            return const LinearGradient(
+            return LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: [Colors.transparent, Colors.black54, Colors.black],
+              colors: [
+                AppColors.black.withAlpha(204),
+                AppColors.black.withAlpha(153),
+                Colors.black
+              ],
               stops: [0.0, 0.5, 1.0],
             ).createShader(rect);
           },
           blendMode: BlendMode.darken,
           child: Image.asset(
-            AppAssets.film1917,
+            bgImage,
             width: double.infinity,
-            height: 500,
+            // height: 500,
             fit: BoxFit.cover,
           ),
         ),
-        Positioned.fill(
-          child: SafeArea(
-            child: Column(
-              children: [
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: Image.asset(
-                    AppAssets.availableNow,
-                    width: double.infinity,
-                    height: 70,
-                    fit: BoxFit.contain,
-                  ),
+        SafeArea(
+          bottom: false,
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Image.asset(
+                  AppAssets.availableNow,
+                  width: 267.w,
+                  //height: 70,
+                  fit: BoxFit.contain,
                 ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 270,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: availableNowMovies.length,
-                    padding: const EdgeInsets.symmetric(horizontal: 32),
-                    itemBuilder: (context, index) {
-                      final isCenter = index == 1;
-                      final movie = availableNowMovies[index];
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 350.h,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: availableNowMovies.length,
+                  onPageChanged: (value) {
+                    _currentIndex = value;
+                    bgImage = availableNowMovies[_currentIndex]['image']!;
+                    setState(() {});
+                  },
+                  itemBuilder: (context, index) {
+                    final movie = availableNowMovies[index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: index == _currentIndex ? 0 : 20),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              movie['image']!,
+                              //width: index == _currentIndex ? 230.w : 180.w,
+                              //height: index == _currentIndex ? 350.h : 270.h,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Positioned(
+                            top: 10,
+                            right: 10,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 6, vertical: 2),
+                              decoration: BoxDecoration(
+                                color: Colors.black.withAlpha(178),
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    size: 14,
+                                    color: AppColors.amber,
+                                  ),
+                                  const SizedBox(width: 2),
+                                  Text(
+                                    movie['rating']!,
+                                    style: GoogleFonts.poppins(
+                                        color: AppColors.white, fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
 
-                      return Padding(
-                        padding: EdgeInsets.only(right: 20, top: isCenter ? 0 : 20),
-                        child: Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.asset(
-                                movie['image']!,
-                                width: isCenter ? 170 : 130,
-                                height: isCenter ? 240 : 210,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                              top: 10,
-                              right: 10,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                                decoration: BoxDecoration(
-                                  color: Colors.black.withOpacity(0.7),
-                                  borderRadius: BorderRadius.circular(6),
-                                ),
-                                child: Row(
-                                  children: [
-                                    const Icon(Icons.star, size: 14,color: AppColors.amber,),
-                                    const SizedBox(width: 2),
-                                    Text(
-                                      movie['rating']!,
-                                      style: GoogleFonts.poppins(color: AppColors.white, fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const WatchNowBanner(),
-              ],
-            ),
+                // ListView.builder(
+                //   scrollDirection: Axis.horizontal,
+                //   itemCount: availableNowMovies.length,
+                //   padding: const EdgeInsets.symmetric(horizontal: 32),
+                //   itemBuilder: (context, index) {
+                //     final isCenter = index == 1;
+                //     final movie = availableNowMovies[index];
+
+                //     return Padding(
+                //       padding:
+                //           EdgeInsets.only(right: 20, top: isCenter ? 0 : 20),
+                //       child: Stack(
+                //         children: [
+                //           ClipRRect(
+                //             borderRadius: BorderRadius.circular(20),
+                //             child: Image.asset(
+                //               movie['image']!,
+                //               width: isCenter ? 170 : 130,
+                //               height: isCenter ? 240 : 210,
+                //               fit: BoxFit.cover,
+                //             ),
+                //           ),
+                //           Positioned(
+                //             top: 10,
+                //             right: 10,
+                //             child: Container(
+                //               padding: const EdgeInsets.symmetric(
+                //                   horizontal: 6, vertical: 2),
+                //               decoration: BoxDecoration(
+                //                 color: Colors.black.withOpacity(0.7),
+                //                 borderRadius: BorderRadius.circular(6),
+                //               ),
+                //               child: Row(
+                //                 children: [
+                //                   const Icon(
+                //                     Icons.star,
+                //                     size: 14,
+                //                     color: AppColors.amber,
+                //                   ),
+                //                   const SizedBox(width: 2),
+                //                   Text(
+                //                     movie['rating']!,
+                //                     style: GoogleFonts.poppins(
+                //                         color: AppColors.white, fontSize: 12),
+                //                   ),
+                //                 ],
+                //               ),
+                //             ),
+                //           ),
+                //         ],
+                //       ),
+                //     );
+                //   },
+                // ),
+              ),
+              const SizedBox(height: 10),
+              const WatchNowBanner(),
+            ],
           ),
         ),
       ],
--- lib/UI/main_layer/tabs/homeTab/widgets/watch_now_banner.dart
+++ lib/UI/main_layer/tabs/homeTab/widgets/watch_now_banner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../../core/utils/app_assets.dart';
 
 class WatchNowBanner extends StatelessWidget {
@@ -7,11 +8,11 @@ class WatchNowBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Image.asset(
         AppAssets.watchNow,
-        width: MediaQuery.of(context).size.width * 0.92,
-        height: 80,
+        width: 354.w,
+        // height: 80,
         fit: BoxFit.contain,
       ),
     );
