207c36d99a31a98f19d2b3c0a0970677dcd1ea86
MohamadQotb <mohamadbakoush@gmail.com>
2025-07-09 18:31:26 +0200

    erorr handled

--- lib/UI/movieDetails/view/views/movie_section_view.dart
+++ lib/UI/movieDetails/view/views/movie_section_view.dart
@@ -11,62 +11,74 @@ class MovieSectionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: 641.h,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(movie.largeCoverImage ?? ''))),
-      child: Container(
-        width: double.infinity,
-        height: 641.h,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [AppColors.black1.withAlpha(200), AppColors.black1],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [.8, 1])),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              SizedBox(
-                height: (248).h,
-              ),
-              Image.asset(
-                AppAssets.playerIcon,
-                width: 97.w,
-              ),
-              Spacer(),
-              Text(
-                movie.title ?? '',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.w700,
-                    color: AppColors.white,
-                    fontFamily: 'Roboto'),
-              ),
-              SizedBox(
-                height: 16.h,
-              ),
-              movie.year == null
-                  ? SizedBox()
-                  : Text(
-                      (movie.year).toString(),
-                      style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700,
-                          color: AppColors.whiteGrey,
-                          fontFamily: 'Roboto'),
-                    ),
-              SizedBox(
-                height: 16.h,
-              ),
-            ],
+      child: Stack(
+        children: [
+          Image.network(
+            movie.largeCoverImage ?? '',
+            width: double.infinity,
+            height: 641.h,
+            fit: BoxFit.cover,
+            errorBuilder: (_, __, ___) => Image.asset(
+              AppAssets.failedImage,
+              fit: BoxFit.fill,
+              height: 641.h,
+              width: double.infinity,
+            ),
           ),
-        ),
+          Container(
+            width: double.infinity,
+            height: 641.h,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [AppColors.black1.withAlpha(200), AppColors.black1],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [.8, 1])),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: (248).h,
+                  ),
+                  Image.asset(
+                    AppAssets.playerIcon,
+                    width: 97.w,
+                  ),
+                  Spacer(),
+                  Text(
+                    movie.title ?? '',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.white,
+                        fontFamily: 'Roboto'),
+                  ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                  movie.year == null
+                      ? SizedBox()
+                      : Text(
+                          (movie.year).toString(),
+                          style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w700,
+                              color: AppColors.whiteGrey,
+                              fontFamily: 'Roboto'),
+                        ),
+                  SizedBox(
+                    height: 16.h,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
