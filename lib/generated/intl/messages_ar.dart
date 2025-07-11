650151f6619c0a8f5f5d8e3f959396eb1738bfd8
Abdulrahman <abdoomran618@gmail.com>
2025-07-11 21:07:49 +0300

    .

--- lib/generated/intl/messages_ar.dart
+++ lib/generated/intl/messages_ar.dart
@@ -22,162 +22,139 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "alreadyHaveAccount": MessageLookupByLibrary.simpleMessage(
-      "لديك حساب بالفعل؟ ",
-    ),
-    "areYouSureYouWantToExit": MessageLookupByLibrary.simpleMessage(
-      "هل أنت متأكد أنك تريد الخروج؟",
-    ),
-    "avatar": MessageLookupByLibrary.simpleMessage("الصورة الرمزية"),
-    "cast": MessageLookupByLibrary.simpleMessage("الممثلين"),
-    "changePassword": MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
-    "character": MessageLookupByLibrary.simpleMessage("الشخصية:"),
-    "confirmPasswordHint": MessageLookupByLibrary.simpleMessage(
-      "تأكيد كلمة المرور",
-    ),
-    "confirmPasswordRequired": MessageLookupByLibrary.simpleMessage(
-      "تأكيد كلمة المرور مطلوب",
-    ),
-    "createAccount": MessageLookupByLibrary.simpleMessage("إنشاء الحساب"),
-    "createOne": MessageLookupByLibrary.simpleMessage("أنشئ حساب"),
-    "deleteAccount": MessageLookupByLibrary.simpleMessage("حذف الحساب"),
-    "deleteAccountMessage": MessageLookupByLibrary.simpleMessage(
-      "هل أنت متأكد أنك تريد حذف الحساب؟",
-    ),
-    "donotHaveAccount": MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟ "),
-    "editProfile": MessageLookupByLibrary.simpleMessage("تعديل الملف الشخصي"),
-    "emailHint": MessageLookupByLibrary.simpleMessage("البريد الإلكتروني"),
-    "emailRequired": MessageLookupByLibrary.simpleMessage(
-      "البريد الإلكتروني مطلوب",
-    ),
-    "exit": MessageLookupByLibrary.simpleMessage("خروج"),
-    "forgetPassword": MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور؟"),
-    "geners": MessageLookupByLibrary.simpleMessage("النوع"),
-    "genreAction": MessageLookupByLibrary.simpleMessage("أكشن"),
-    "genreAdventure": MessageLookupByLibrary.simpleMessage("مغامرة"),
-    "genreAnimation": MessageLookupByLibrary.simpleMessage("رسوم متحركة"),
-    "genreBiography": MessageLookupByLibrary.simpleMessage("سيرة ذاتية"),
-    "genreComedy": MessageLookupByLibrary.simpleMessage("كوميديا"),
-    "genreCrime": MessageLookupByLibrary.simpleMessage("جريمة"),
-    "genreDocumentary": MessageLookupByLibrary.simpleMessage("وثائقي"),
-    "genreDrama": MessageLookupByLibrary.simpleMessage("دراما"),
-    "genreFamily": MessageLookupByLibrary.simpleMessage("عائلي"),
-    "genreFantasy": MessageLookupByLibrary.simpleMessage("خيال"),
-    "genreFilmNoir": MessageLookupByLibrary.simpleMessage("فيلم نوار"),
-    "genreGameShow": MessageLookupByLibrary.simpleMessage("برامج ألعاب"),
-    "genreHistory": MessageLookupByLibrary.simpleMessage("تاريخي"),
-    "genreHorror": MessageLookupByLibrary.simpleMessage("رعب"),
-    "genreMusic": MessageLookupByLibrary.simpleMessage("موسيقى"),
-    "genreMusical": MessageLookupByLibrary.simpleMessage("غنائي"),
-    "genreMystery": MessageLookupByLibrary.simpleMessage("غموض"),
-    "genreNews": MessageLookupByLibrary.simpleMessage("أخبار"),
-    "genreRealityTV": MessageLookupByLibrary.simpleMessage("تلفزيون الواقع"),
-    "genreRomance": MessageLookupByLibrary.simpleMessage("رومانسي"),
-    "genreSciFi": MessageLookupByLibrary.simpleMessage("خيال علمي"),
-    "genreShort": MessageLookupByLibrary.simpleMessage("قصير"),
-    "genreSport": MessageLookupByLibrary.simpleMessage("رياضي"),
-    "genreTalkShow": MessageLookupByLibrary.simpleMessage("برنامج حواري"),
-    "genreThriller": MessageLookupByLibrary.simpleMessage("إثارة"),
-    "genreWar": MessageLookupByLibrary.simpleMessage("حرب"),
-    "genreWestern": MessageLookupByLibrary.simpleMessage("ويسترن"),
-    "googleCancelled": MessageLookupByLibrary.simpleMessage(
-      "تم إلغاء تسجيل الدخول عبر جوجل",
-    ),
-    "history": MessageLookupByLibrary.simpleMessage("السجل"),
-    "invalidEmail": MessageLookupByLibrary.simpleMessage(
-      "أدخل بريد إلكتروني صحيح",
-    ),
-    "login": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
-    "loginSuccess": MessageLookupByLibrary.simpleMessage(
-      "تم تسجيل الدخول بنجاح",
-    ),
-    "loginWithGoogle": MessageLookupByLibrary.simpleMessage(
-      "تسجيل الدخول بواسطة جوجل",
-    ),
-    "name": MessageLookupByLibrary.simpleMessage("الاسم:"),
-    "nameHint": MessageLookupByLibrary.simpleMessage("الاسم"),
-    "nameRequired": MessageLookupByLibrary.simpleMessage("الاسم مطلوب"),
-    "newPasswordHint": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور الجديدة",
-    ),
-    "no": MessageLookupByLibrary.simpleMessage("لا"),
-    "nocast": MessageLookupByLibrary.simpleMessage("لا يوجد ممثلين متاحين"),
-    "nocharacter": MessageLookupByLibrary.simpleMessage("لا توجد شخصية متاحة"),
-    "nomovies": MessageLookupByLibrary.simpleMessage("لا توجد أفلام!"),
-    "noname": MessageLookupByLibrary.simpleMessage("لا يوجد اسم متاح"),
-    "nosummary": MessageLookupByLibrary.simpleMessage(
-      "لا يوجد ملخص متاح لهذا الفيلم!",
-    ),
-    "oldPasswordHint": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور القديمة",
-    ),
-    "or": MessageLookupByLibrary.simpleMessage("أو"),
-    "passwordHint": MessageLookupByLibrary.simpleMessage("كلمة المرور"),
-    "passwordMinLength": MessageLookupByLibrary.simpleMessage(
-      "يجب أن تكون كلمة المرور 6 أحرف على الأقل",
-    ),
-    "passwordNumber": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور يجب أن تحتوي على رقم واحد على الأقل",
-    ),
-    "passwordRequired": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور مطلوبة",
-    ),
-    "passwordShouldNotMatch": MessageLookupByLibrary.simpleMessage(
-      "لا يجب ان تكون كلمه المرور نفس القديمه ",
-    ),
-    "passwordSpecial": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور يجب أن تحتوي على رمز خاص واحد على الأقل",
-    ),
-    "passwordUppercase": MessageLookupByLibrary.simpleMessage(
-      "كلمة المرور يجب أن تحتوي على حرف كبير واحد على الأقل",
-    ),
-    "passwordsDoNotMatch": MessageLookupByLibrary.simpleMessage(
-      "كلمتا المرور غير متطابقتين",
-    ),
-    "phoneExactLength": MessageLookupByLibrary.simpleMessage(
-      "رقم الهاتف يجب أن يحتوي على 13 رقمًا بالضبط",
-    ),
-    "phoneHint": MessageLookupByLibrary.simpleMessage("رقم الهاتف"),
-    "phoneOnlyDigits": MessageLookupByLibrary.simpleMessage(
-      "رقم الهاتف يجب أن يحتوي على أرقام فقط بعد +",
-    ),
-    "phoneRequired": MessageLookupByLibrary.simpleMessage("رقم الهاتف مطلوب"),
-    "phoneStartWithPlus": MessageLookupByLibrary.simpleMessage(
-      "يجب أن يبدأ رقم الهاتف بعلامة +",
-    ),
-    "pickAvatar": MessageLookupByLibrary.simpleMessage("اختر صورة رمزية"),
-    "profileDeleted": MessageLookupByLibrary.simpleMessage(
-      "تم حذف الحساب بنجاح",
-    ),
-    "profileUpdateFailed": MessageLookupByLibrary.simpleMessage(
-      "فشل التحديث: ",
-    ),
-    "profileUpdated": MessageLookupByLibrary.simpleMessage(
-      "تم تحديث الملف الشخصي بنجاح",
-    ),
-    "register": MessageLookupByLibrary.simpleMessage("تسجيل"),
-    "registrationFailed": MessageLookupByLibrary.simpleMessage(
-      "فشل في إنشاء الحساب",
-    ),
-    "registrationSuccess": MessageLookupByLibrary.simpleMessage(
-      "تم إنشاء الحساب بنجاح",
-    ),
-    "resetPassword": MessageLookupByLibrary.simpleMessage(
-      "إعادة تعيين كلمة المرور",
-    ),
-    "screenshots": MessageLookupByLibrary.simpleMessage("لقطات الشاشة"),
-    "search": MessageLookupByLibrary.simpleMessage("ابحث"),
-    "seeMore": MessageLookupByLibrary.simpleMessage("عرض المزيد"),
-    "similar": MessageLookupByLibrary.simpleMessage("مشابه"),
-    "summary": MessageLookupByLibrary.simpleMessage("الملخص"),
-    "updateData": MessageLookupByLibrary.simpleMessage("تحديث البيانات"),
-    "userName": MessageLookupByLibrary.simpleMessage("اسم المستخدم"),
-    "watch": MessageLookupByLibrary.simpleMessage("مشاهدة"),
-    "watchList": MessageLookupByLibrary.simpleMessage("قائمة المشاهدة"),
-    "wishList": MessageLookupByLibrary.simpleMessage("قائمة الرغبات"),
-    "yes": MessageLookupByLibrary.simpleMessage("نعم"),
-    "youAreGoingToWatchTheMovie": MessageLookupByLibrary.simpleMessage(
-      "أنت ذاهب لمشاهدة الفيلم",
-    ),
-  };
+        "alreadyHaveAccount":
+            MessageLookupByLibrary.simpleMessage("لديك حساب بالفعل؟ "),
+        "areYouSureYouWantToExit": MessageLookupByLibrary.simpleMessage(
+            "هل أنت متأكد أنك تريد الخروج؟"),
+        "avatar": MessageLookupByLibrary.simpleMessage("الصورة الرمزية"),
+        "cast": MessageLookupByLibrary.simpleMessage("الممثلين"),
+        "changePassword":
+            MessageLookupByLibrary.simpleMessage("تغيير كلمة المرور"),
+        "character": MessageLookupByLibrary.simpleMessage("الشخصية:"),
+        "confirmPasswordHint":
+            MessageLookupByLibrary.simpleMessage("تأكيد كلمة المرور"),
+        "confirmPasswordRequired":
+            MessageLookupByLibrary.simpleMessage("تأكيد كلمة المرور مطلوب"),
+        "createAccount": MessageLookupByLibrary.simpleMessage("إنشاء الحساب"),
+        "createOne": MessageLookupByLibrary.simpleMessage("أنشئ حساب"),
+        "deleteAccount": MessageLookupByLibrary.simpleMessage("حذف الحساب"),
+        "deleteAccountMessage": MessageLookupByLibrary.simpleMessage(
+            "هل أنت متأكد أنك تريد حذف الحساب؟"),
+        "donotHaveAccount":
+            MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟ "),
+        "editProfile":
+            MessageLookupByLibrary.simpleMessage("تعديل الملف الشخصي"),
+        "emailHint": MessageLookupByLibrary.simpleMessage("البريد الإلكتروني"),
+        "emailRequired":
+            MessageLookupByLibrary.simpleMessage("البريد الإلكتروني مطلوب"),
+        "exit": MessageLookupByLibrary.simpleMessage("خروج"),
+        "forgetPassword":
+            MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور؟"),
+        "geners": MessageLookupByLibrary.simpleMessage("النوع"),
+        "genreAction": MessageLookupByLibrary.simpleMessage("أكشن"),
+        "genreAdventure": MessageLookupByLibrary.simpleMessage("مغامرة"),
+        "genreAnimation": MessageLookupByLibrary.simpleMessage("رسوم متحركة"),
+        "genreBiography": MessageLookupByLibrary.simpleMessage("سيرة ذاتية"),
+        "genreComedy": MessageLookupByLibrary.simpleMessage("كوميديا"),
+        "genreCrime": MessageLookupByLibrary.simpleMessage("جريمة"),
+        "genreDocumentary": MessageLookupByLibrary.simpleMessage("وثائقي"),
+        "genreDrama": MessageLookupByLibrary.simpleMessage("دراما"),
+        "genreFamily": MessageLookupByLibrary.simpleMessage("عائلي"),
+        "genreFantasy": MessageLookupByLibrary.simpleMessage("خيال"),
+        "genreFilmNoir": MessageLookupByLibrary.simpleMessage("فيلم نوار"),
+        "genreGameShow": MessageLookupByLibrary.simpleMessage("برامج ألعاب"),
+        "genreHistory": MessageLookupByLibrary.simpleMessage("تاريخي"),
+        "genreHorror": MessageLookupByLibrary.simpleMessage("رعب"),
+        "genreMusic": MessageLookupByLibrary.simpleMessage("موسيقى"),
+        "genreMusical": MessageLookupByLibrary.simpleMessage("غنائي"),
+        "genreMystery": MessageLookupByLibrary.simpleMessage("غموض"),
+        "genreNews": MessageLookupByLibrary.simpleMessage("أخبار"),
+        "genreRealityTV":
+            MessageLookupByLibrary.simpleMessage("تلفزيون الواقع"),
+        "genreRomance": MessageLookupByLibrary.simpleMessage("رومانسي"),
+        "genreSciFi": MessageLookupByLibrary.simpleMessage("خيال علمي"),
+        "genreShort": MessageLookupByLibrary.simpleMessage("قصير"),
+        "genreSport": MessageLookupByLibrary.simpleMessage("رياضي"),
+        "genreTalkShow": MessageLookupByLibrary.simpleMessage("برنامج حواري"),
+        "genreThriller": MessageLookupByLibrary.simpleMessage("إثارة"),
+        "genreWar": MessageLookupByLibrary.simpleMessage("حرب"),
+        "genreWestern": MessageLookupByLibrary.simpleMessage("ويسترن"),
+        "googleCancelled": MessageLookupByLibrary.simpleMessage(
+            "تم إلغاء تسجيل الدخول عبر جوجل"),
+        "history": MessageLookupByLibrary.simpleMessage("السجل"),
+        "invalidEmail":
+            MessageLookupByLibrary.simpleMessage("أدخل بريد إلكتروني صحيح"),
+        "login": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+        "loginSuccess":
+            MessageLookupByLibrary.simpleMessage("تم تسجيل الدخول بنجاح"),
+        "loginWithGoogle":
+            MessageLookupByLibrary.simpleMessage("تسجيل الدخول بواسطة جوجل"),
+        "name": MessageLookupByLibrary.simpleMessage("الاسم:"),
+        "nameHint": MessageLookupByLibrary.simpleMessage("الاسم"),
+        "nameRequired": MessageLookupByLibrary.simpleMessage("الاسم مطلوب"),
+        "newPasswordHint":
+            MessageLookupByLibrary.simpleMessage("كلمة المرور الجديدة"),
+        "no": MessageLookupByLibrary.simpleMessage("لا"),
+        "nocast": MessageLookupByLibrary.simpleMessage("لا يوجد ممثلين متاحين"),
+        "nocharacter":
+            MessageLookupByLibrary.simpleMessage("لا توجد شخصية متاحة"),
+        "nomovies": MessageLookupByLibrary.simpleMessage("لا توجد أفلام!"),
+        "noname": MessageLookupByLibrary.simpleMessage("لا يوجد اسم متاح"),
+        "nosummary": MessageLookupByLibrary.simpleMessage(
+            "لا يوجد ملخص متاح لهذا الفيلم!"),
+        "oldPasswordHint":
+            MessageLookupByLibrary.simpleMessage("كلمة المرور القديمة"),
+        "or": MessageLookupByLibrary.simpleMessage("أو"),
+        "passwordHint": MessageLookupByLibrary.simpleMessage("كلمة المرور"),
+        "passwordMinLength": MessageLookupByLibrary.simpleMessage(
+            "يجب أن تكون كلمة المرور 6 أحرف على الأقل"),
+        "passwordNumber": MessageLookupByLibrary.simpleMessage(
+            "كلمة المرور يجب أن تحتوي على رقم واحد على الأقل"),
+        "passwordRequired":
+            MessageLookupByLibrary.simpleMessage("كلمة المرور مطلوبة"),
+        "passwordShouldNotMatch": MessageLookupByLibrary.simpleMessage(
+            "لا يجب ان تكون كلمه المرور نفس القديمه "),
+        "passwordSpecial": MessageLookupByLibrary.simpleMessage(
+            "كلمة المرور يجب أن تحتوي على رمز خاص واحد على الأقل"),
+        "passwordUppercase": MessageLookupByLibrary.simpleMessage(
+            "كلمة المرور يجب أن تحتوي على حرف كبير واحد على الأقل"),
+        "passwordsDoNotMatch":
+            MessageLookupByLibrary.simpleMessage("كلمتا المرور غير متطابقتين"),
+        "phoneExactLength": MessageLookupByLibrary.simpleMessage(
+            "رقم الهاتف يجب أن يحتوي على 13 رقمًا بالضبط"),
+        "phoneHint": MessageLookupByLibrary.simpleMessage("رقم الهاتف"),
+        "phoneOnlyDigits": MessageLookupByLibrary.simpleMessage(
+            "رقم الهاتف يجب أن يحتوي على أرقام فقط بعد +"),
+        "phoneRequired":
+            MessageLookupByLibrary.simpleMessage("رقم الهاتف مطلوب"),
+        "phoneStartWithPlus": MessageLookupByLibrary.simpleMessage(
+            "يجب أن يبدأ رقم الهاتف بعلامة +"),
+        "pickAvatar": MessageLookupByLibrary.simpleMessage("اختر صورة رمزية"),
+        "profileDeleted":
+            MessageLookupByLibrary.simpleMessage("تم حذف الحساب بنجاح"),
+        "profileUpdateFailed":
+            MessageLookupByLibrary.simpleMessage("فشل التحديث: "),
+        "profileUpdated":
+            MessageLookupByLibrary.simpleMessage("تم تحديث الملف الشخصي بنجاح"),
+        "register": MessageLookupByLibrary.simpleMessage("تسجيل"),
+        "registrationFailed":
+            MessageLookupByLibrary.simpleMessage("فشل في إنشاء الحساب"),
+        "registrationSuccess":
+            MessageLookupByLibrary.simpleMessage("تم إنشاء الحساب بنجاح"),
+        "resetPassword":
+            MessageLookupByLibrary.simpleMessage("إعادة تعيين كلمة المرور"),
+        "screenshots": MessageLookupByLibrary.simpleMessage("لقطات الشاشة"),
+        "search": MessageLookupByLibrary.simpleMessage("ابحث"),
+        "seeMore": MessageLookupByLibrary.simpleMessage("عرض المزيد"),
+        "similar": MessageLookupByLibrary.simpleMessage("مشابه"),
+        "summary": MessageLookupByLibrary.simpleMessage("الملخص"),
+        "updateData": MessageLookupByLibrary.simpleMessage("تحديث البيانات"),
+        "userName": MessageLookupByLibrary.simpleMessage("اسم المستخدم"),
+        "watch": MessageLookupByLibrary.simpleMessage("مشاهدة"),
+        "watchList": MessageLookupByLibrary.simpleMessage("قائمة المشاهدة"),
+        "wishList": MessageLookupByLibrary.simpleMessage("قائمة الرغبات"),
+        "yes": MessageLookupByLibrary.simpleMessage("نعم"),
+        "youAreGoingToWatchTheMovie":
+            MessageLookupByLibrary.simpleMessage("أنت ذاهب لمشاهدة الفيلم")
+      };
 }
